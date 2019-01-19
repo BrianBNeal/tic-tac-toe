68ca164b72c08748ca10f5afddcc6d89cc1d45ee
Brian Neal <brianbneal@gmail.com>
2019-01-19 16:49:20 -0600

    added data for win conditions and player moves

---
+++ data.js
@@ -0,0 +1,13 @@
+const winConditions = [
+    ["square0", "square1", "square2"],
+    ["square3", "square4", "square5"],
+    ["square6", "square7", "square8"],
+    ["square0", "square3", "square6"],
+    ["square1", "square4", "square7"],
+    ["square2", "square5", "square8"],
+    ["square0", "square4", "square8"],
+    ["square2", "square4", "square6"],
+]
+
+let xSquares = []
+let oSquares = []
--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 // #1 and #2 are repeated until a row of 3 of the same character is created
+/* Array of all win conditions. Array of all O moves.  Array of all X moves.  Loop at end of each turn to compare moves with win conditions.  Length of X moves array is 5 and there is no win, then it's a draw. */
+
 // When a win happens:
 // Alert the players who won
 // Disallow any further clicks on the game board
@@ -7,8 +9,24 @@
 // Alert players that game has ended in a tie
 // Display a Play Again button that resets the game
 
-//Turn Counter to determine which letter to place
-let turnCounter = 1
+let turnCounter = 0
+
+const createGame = () => {
+    //Begins turnCounter at 1 each time a new game is created
+    turnCounter = 1
+
+    //Loop to create 9 squares for gameboard
+    for (let i = 0; i < 9; i++) {
+        const container = document.querySelector("#grid__container")
+        container.innerHTML += `<div id="square${i}" class="square"></div>`
+    }
+}
+
+const checkForWin = () => {
+    
+}
+
+createGame();
 
 // Create reference to the game spaces
 let allSquares = document.getElementsByClassName("square")
@@ -20,11 +38,18 @@ for (const square of allSquares) {
         (event) => {
             
 
-            //add a div with a letter inside to the chosen square
-            if (turnCounter % 2 === 0 && turnCounter < 10) {
+            //even turns are player O, and square must be empty
+            if (turnCounter % 2 === 0 && square.innerHTML === "") {
+                //add letter to the square
                 square.innerHTML = `<p class="o">O</p>`
+                //add move to collection of moves
+                oSquares.push(square)
+                //increment to next turn
                 turnCounter += 1
-            } else if (turnCounter < 10) {
+                console.log(oSquares)
+
+            //odd turns are player X    
+            } else if (turnCounter < 10 && square.innerHTML === "") {
                 square.innerHTML = `<p class="x">X</p>`
                 turnCounter += 1
             }
@@ -33,6 +58,6 @@ for (const square of allSquares) {
 }
 
 //Announce the end of the game
-if (turnCounter === 10) {
+if (turnCounter === 9) {
     window.alert("It was a tie!")
 }
